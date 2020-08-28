d63766b8bdcf8577bee29aba6eaa1b21b8d0d93d
Redheart <alisa-book@yandex.ru>
2020-08-28 20:17:17 +0300

    main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import com.sun.net.httpserver.HttpServer;
+import risk.RiskHandler;
+import status.StatusHandler;
+import tags.TagHandler;
 import user.UserHandler;
 
 import java.net.InetSocketAddress;
@@ -7,8 +10,21 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
         server.createContext(UserHandler.USER, new UserHandler());
         server.createContext(UserHandler.USERS, new UserHandler());
+        server.createContext(UserHandler.AUTHORIZATION, new UserHandler());
+
+        server.createContext(RiskHandler.RISK, new RiskHandler());
+        server.createContext(RiskHandler.RISKS, new RiskHandler());
+        server.createContext(RiskHandler.RISKSBYUSER, new RiskHandler());
+
+        server.createContext(StatusHandler.STATUSES, new StatusHandler());
+
+        server.createContext(TagHandler.TAG, new TagHandler());
+        server.createContext(TagHandler.TAGS, new TagHandler());
+        server.createContext(TagHandler.RISKSFORTAG, new TagHandler());
+
         server.setExecutor(null); // creates a default executor
         server.start();
     }
